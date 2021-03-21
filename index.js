c6a36a1d17b7c3f5915e32d97574c653fa6b3823
SpheeresX <76824533+SpheeresX@users.noreply.github.com>
2021-03-21 23:06:26 +0100

    turn the desktop app into a cli app
    yue/yode has many issues and i hate electron so i rewrote the project and now it's a cli app

--- index.js
+++ index.js
@@ -1,86 +1,60 @@
-const gui = require('gui')
 const fetch = require('node-fetch');
 const sjcl = require('sjcl');
+const { Command } = require('commander');
 const crypto = require('crypto');
-const { Worker } = require('worker_threads');
+const program = new Command();
 
-const instanceURL = "https://yeeturl.glitch.me/";
-
-function validateURL() {
-  const urlSchema = /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i;
-  return urlSchema.test(value);
+function validateURL(value) {
+	const urlSchema = /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i;
+	return urlSchema.test(value);
 }
 
-// create window
-const win = gui.Window.create({})
-win.setContentSize({width: 350, height: 93})
-win.onClose = () => gui.MessageLoop.quit();
+const config = {
+	instanceURL: 'https://yeeturl.glitch.me' // DON'T add a slash at the end of the url!
+}
 
-const contentView = gui.Container.create();
-contentView.setStyle({flexDirection: 'column'});
-win.setContentView(contentView);
+program
+	.version('0.0.1')
+	.option('-s, --shorten <url>', 'shorten a url')
+	.option('-g, --get <url>', 'get the long url from a shortened link')
+	.parse();
+var opts = program.opts();
 
-const logo = gui.Label.create('yeeturl');
-contentView.addChildView(logo);
-
-const urlInput = gui.Entry.create();
-contentView.addChildView(urlInput);
-
-const shortenButton = gui.Button.create('Shorten');
-contentView.addChildView(shortenButton);
-
-shortenButton.onClick = () => {
-	const url = urlInput.getText();
-	if (!validateURL(url)) return;
-	urlInput.setText('Shortening...');
-
-	// create a new worker that will do all synchronous crypto work without blocking the main thread
-	const worker = new Worker(`
-		const { parentPort } = require('worker_threads');
-
-		const fetch = require('node-fetch');
-		const sjcl = require('sjcl');
-		const crypto = require('crypto');
-
-		parentPort.once('message', async message => {
-			// "message" is the url
-			const password = crypto.randomBytes(5).toString('hex');
-			const encrypted = sjcl.encrypt(password, message, { iter: 275000 });
-
-			try {
-				// send it to the server
-				const res = await fetch('${instanceURL}/api/shorten', {
-					method: 'POST',
-					headers: {
-					  'Accept': 'application/json',
-					  'Content-Type': 'application/json'
-					},
-					body: JSON.stringify({
-						url: encrypted
-					})
-				});
-				if (!res.ok) throw new Error();
-
-				const json = await res.json();
-
-				return parentPort.postMessage({ pw: password, id: json.link });
-			} catch {
-				return parentPort.postMessage({ err: 'An error has occured.' });
-			}
+if (opts.shorten) {
+	(async () => {
+		// check if the url is valid
+		if (!validateURL(opts.shorten))
+			return console.log('This URL is invalid.');
+		// if it is, generate a password & encrypt it
+		console.log('Encrypting - this may take a while...');
+		var password = crypto.randomBytes(6).toString('hex');
+		var encrypted = sjcl.encrypt(password, opts.shorten, { iter: 275000 });
+		// upload the encrypted url to the server
+		const res = await fetch(`${config.instanceURL}/api/shorten`, {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				url: encrypted
+			})
 		});
-	`, { eval: true });  
-	worker.postMessage(url);  
 
-	worker.on('message', msg => {
-		if (msg.err) return urlInput.setText('An error has occured.');
-		urlInput.setText(`${instanceURL}/#${msg.id}/${msg.pw}`);
-	});
-}
-
-win.center();
-win.activate();
-
-if (!process.versions.yode) {
-  gui.MessageLoop.run()
-  process.exit(0)
+		if (!res.ok) {
+			switch (res.status) {
+				case 429:
+					return console.error('You are sending too much requests. Please try again later.');
+				case 413:
+					return console.error('The link you have provided is too long.');
+				default:
+					return console.error('An unknown error has occured.');
+			}
+		}
+		var data = await res.json();
+		var shortenedURL = `${config.instanceURL}/#${data.link}/${password}`;
+		console.log(`Done! ${shortenedURL}`);
+	})();
+} else if (opts.get) {
+	console.log('This feature hasn\'t been implemented yet.')
 }
