d28b861ab6c33de05cae1c5ff337082e8a8cb7dc
SpheeresX <76824533+SpheeresX@users.noreply.github.com>
2021-03-22 08:37:18 +0100

    implement --get and add terminal styling (chalk)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fetch = require('node-fetch');
 const sjcl = require('sjcl');
 const { Command } = require('commander');
 const crypto = require('crypto');
+const chalk = require('chalk');
 const program = new Command();
 
 function validateURL(value) {
@@ -9,8 +10,13 @@ function validateURL(value) {
 	return urlSchema.test(value);
 }
 
+// if you're self-hosting yeeturl, change this configuration
 const config = {
-	instanceURL: 'https://yeeturl.glitch.me' // DON'T add a slash at the end of the url!
+	// the instance yeeturl-desktop will use to shorten/get urls
+	instanceURL: 'https://yeeturl.glitch.me', // DON'T add a slash at the end of the url!
+	// yeeturl instances that the user can get original/long urls from
+	// this can be helpful if you're running mirror sites
+	supportedInstances: ['yeeturl.glitch.me']
 }
 
 program
@@ -24,10 +30,10 @@ if (opts.shorten) {
 	(async () => {
 		// check if the url is valid
 		if (!validateURL(opts.shorten))
-			return console.log('This URL is invalid.');
+			return console.error(chalk.red('This URL is invalid.'));
 		// if it is, generate a password & encrypt it
-		console.log('Encrypting - this may take a while...');
-		var password = crypto.randomBytes(6).toString('hex');
+		console.log(chalk.magentaBright('Encrypting - this may take a while...'));
+		var password = crypto.randomBytes(5).toString('hex');
 		var encrypted = sjcl.encrypt(password, opts.shorten, { iter: 275000 });
 		// upload the encrypted url to the server
 		const res = await fetch(`${config.instanceURL}/api/shorten`, {
@@ -44,17 +50,61 @@ if (opts.shorten) {
 		if (!res.ok) {
 			switch (res.status) {
 				case 429:
-					return console.error('You are sending too much requests. Please try again later.');
+					return console.error(chalk.red('You are sending too much requests. Please try again later.'));
 				case 413:
-					return console.error('The link you have provided is too long.');
+					return console.error(chalk.red('The link you have provided is too long.'));
 				default:
-					return console.error('An unknown error has occured.');
+					return console.error(chalk.red('An unknown error has occured.'));
 			}
 		}
 		var data = await res.json();
 		var shortenedURL = `${config.instanceURL}/#${data.link}/${password}`;
-		console.log(`Done! ${shortenedURL}`);
+		console.log(chalk.green.bold('Done! ') + chalk.green(shortenedURL));
 	})();
 } else if (opts.get) {
-	console.log('This feature hasn\'t been implemented yet.')
+	(async () => {
+		// check if the url is valid
+		if (!validateURL(opts.get))
+			return console.error(chalk.red('This URL is invalid.'));
+		// parse the url & make sure this instance is supported
+		var parsed = new URL(opts.get);
+		if (!config.supportedInstances.includes(parsed.hostname))
+			return console.error(chalk.red('This instance is unsupported.'));
+		// get the id & password from the url
+		// code[0] is the id of the short link,
+		// and code[1] is the password we're decrypting the url with
+		const code = parsed.hash.replace("#", "").split("/");
+		// get the encrypted url from the server
+		console.log(chalk.magentaBright('Getting the encrypted url...'));
+		const url = `${config.instanceURL}/api/getlink?id=${encodeURIComponent(code[0])}`;
+		const res = await fetch(url);
+		if (!res.ok) {
+			console.log(res.status);
+			switch (res.status) {
+				case 404:
+					return console.error(chalk.red("We couldn't find this link! It may have been removed."));
+				case 429:
+					return console.error(chalk.red("You are sending too many requests. Try again in a few minutes."));
+				default:
+					return console.error(chalk.red("An unknown error has occured while getting this encrypted URL."));
+			}
+		}
+		const data = await res.text();
+		// decrypt it, lmk if there's a better way to do this
+		console.log(chalk.magentaBright('Decrypting...'));
+		var decrypted;
+		try {
+			decrypted = sjcl.decrypt(code[1], data);
+		} catch {
+			// if the data is invalid, or the password is incorrect, catch the error.
+			return console.error(chalk.red('An error has occured while decrypting this link. This often happens when the password (short link) is invalid - make sure to check for any typos.'));
+		}
+		// validate the url
+		if (!validateURL(decrypted))
+			return console.error(chalk.red('The URL you were supposed to get redirected to is invalid and has been blocked to prevent attacks.'));
+		// if everything's good, show the url to the user.
+		console.log(chalk.green.bold('Done! ') + chalk.green(decrypted));
+	})();
+} else {
+	console.log('Use --help to view available commands');
 }
